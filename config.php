72ac6a0daed2dde59e1064a78f4fff466dc115d7
LemjidMed <130568110+LemjidMed@users.noreply.github.com>
2025-12-23 10:18:57 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$baseURL = 'http://localhost/LinkedIn-API-Login-PHP/';				// base or site url is the site where code 																		linked in button is kept for signup
+$callbackURL = 'http://localhost/LinkedIn-API-Login-PHP/process.php';	//callback or redirect url is the page you 																	want to open after successful getting of data
+																//i.e. index.php page (must be same in linkedin dashboard)
+$linkedinApiKey = '';								//APP ID(will receive from linkedin dashboard)
+$linkedinApiSecret = 'WPL_AP1.cOdmxBZwXjXMYmxq.Y0Rmzg==';						//APP Client 
+$linkedinScope = 'profile';				//This is fixed no need to change
+?>
+
